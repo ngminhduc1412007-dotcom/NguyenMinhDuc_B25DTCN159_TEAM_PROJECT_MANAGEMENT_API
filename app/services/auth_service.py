7c5ce03ecbb8e6455aac3bc905164f694c1bd3b0
mduc <ngminhduc1412007@gmail.com>
2026-08-27 16:07:46 +0700

    update validate auth

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,10 +1,8 @@
+import re
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.auth import(
-    UserRegisterRequest,
-    UserLoginRequest
-)
+from app.schemas.auth import UserRegisterRequest, UserLoginRequest
 from app.core.security import(
     hash_password, 
     verify_password,
@@ -24,6 +22,26 @@ def register_service(user: UserRegisterRequest, db: Session):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already exists"
         )
+        
+    if len(user.password) < 8:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password must be at least 8 characters long"
+        )
+
+    if not re.search(r'\d', user.password): # \d đại diện cho các số 0-9
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password must contain digits"
+        )
+
+    # Kiểm tra phải chứa ít nhất 1 ký tự đặc biệt
+    if not re.search(r'[@#$%!^&*()_\-+=|<>/,]', user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password must contain special characters"
+        )
+        
     hashed_password = hash_password(user.password)
     new_user = User(
         full_name = user.username,
@@ -42,7 +60,7 @@ def login_service(user: UserLoginRequest, db: Session):
     if not user_db:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid email"
+            detail="Invalid login"
         )
 
     if not user_db.is_active:
